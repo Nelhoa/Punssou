c5cc24c1048636f6bad777df69b84b4652c56e86
Neil Savin <savin.neil.56@gmail.com>
2024-05-22 19:21:07 +0200

    Improve Player sélection display

--- src/lib/components/Punto/PlayerSelection.svelte
+++ src/lib/components/Punto/PlayerSelection.svelte
@@ -13,6 +13,7 @@
 
 	let players = $state<PuntoPlayer[]>([...givenPlayers] ?? []);
 	let newName = $state<string>('');
+	let disableAddPlayer = $derived(players.length >= 4 || newName.length < 1);
 
 	function newPlayer(name: string) {
 		players.push(new PuntoPlayer(name));
@@ -21,52 +22,62 @@
 	function inputMount(e: HTMLInputElement) {
 		wait(200).then(() => e.focus());
 	}
+
+	function addPlayer() {
+		if (disableAddPlayer) return;
+		newPlayer(newName);
+		newName = '';
+	}
+
+	function onkeydown(e: KeyboardEvent) {
+		if (e.key === 'Enter') addPlayer();
+	}
 </script>
 
-<div class="w-full h-screen flex flex-col gap-3 items-center justify-center">
-	<div class="flex flex-col gap-1">
+<div class="w-full h-screen flex flex-col gap-3 items-center justify-center bg-[--surface-color-1]">
+	<div class="text-3xl mb-3 font-bold">Le Punto !</div>
+	<div class="flex flex-col gap-1 w-[90%] max-w-[250px]">
 		{#each players as player}
-			<div class="grid grid-cols-[1fr_auto]">
+			<div class="grid grid-cols-[1fr_auto] gap-3">
 				<input
-					class="w-[90%] max-w-[200px] px-3 py-1 bg-blue-500 text-white font-semibold rounded"
+					class="w-full px-3 py-1 bg-[--surface-color-2] text-black/85 hover:bg-[--surface-color-3] focus:shadow font-semibold rounded"
 					type="text"
 					bind:value={player.name}
 				/>
 				<button
 					onclick={() => (players = players.filter((p) => p !== player))}
-					class="px-3 py-1 bg-red-500 text-white font-semibold rounded">Suppr.</button
+					class="px-3 py-1 bg-red-500/80 hover:bg-red-500 text-white font-semibold rounded text-[13px]"
+					>Suppr.</button
 				>
 			</div>
 		{/each}
 	</div>
-
-	<div>
+	<div class="grid grid-cols-[1fr_auto] w-[90%] max-w-[250px] gap-3">
 		<input
 			use:inputMount
-			class="border rounded px-3 py-1 focus:outline-none focus-visible:shadow"
+			{onkeydown}
+			class="border border-black/15 bg-white/80 hover:bg-white focus-visible:bg-white w-full rounded px-3 py-1 focus:outline-none focus-visible:shadow"
 			disabled={players.length >= 4}
 			bind:value={newName}
 			type="text"
 		/>
 
 		<button
-			disabled={players.length >= 4 || newName.length < 1}
-			onclick={() => {
-				newPlayer(newName);
-				newName = '';
-			}}
-			class="border px-3 py-1 rounded text-sm font-semibold disabled:opacity-30">Ajouter</button
+			disabled={disableAddPlayer}
+			onclick={addPlayer}
+			class="border w-full border-black/20 bg-[--surface-color-3] px-3 py-1 rounded text-[13px] font-semibold disabled:opacity-30"
+			>Ajouter</button
 		>
 	</div>
 
-	<div>
+	<div class="flex gap-2 my-2">
 		<input type="checkbox" bind:checked={settings.showNumber} />
-		Montrer les nombres
+		<div class="font-semibold text-[15px] text-black/70">Montrer les nombres</div>
 	</div>
 
 	{#if onstart}
 		<button
-			class="bg-cyan-600 disabled:bg-gray-300 text-white font-semibold rounded px-3 py-1 transition-transform hover:scale-110 disabled:hover:scale-100"
+			class="bg-blue-500 text-lg disabled:bg-gray-300 text-white font-semibold rounded px-3 py-1 transition-transform hover:scale-110 disabled:hover:scale-100"
 			disabled={players.length < 2}
 			onclick={() => onstart(players)}>Démarrer</button
 		>
